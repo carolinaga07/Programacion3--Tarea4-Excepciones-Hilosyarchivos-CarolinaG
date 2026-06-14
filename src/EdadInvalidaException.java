757b5a7dfcc3f2184bdca0534b73eef4d1ab82e4
carolina <carolina@MONNIE>
2026-06-14 18:24:24 -0400

    edicion de clases para el funcionamiento

--- src/EdadInvalidaException.java
+++ src/EdadInvalidaException.java
@@ -3,12 +3,5 @@ public class EdadInvalidaException extends Exception{
     public EdadInvalidaException(String message) {
         super(message);
     }
-
-    public void ValidarEdad(int edad) throws EdadInvalidaException{
-        if(edad < 18){
-            throw new EdadInvalidaException("Debe ser mayor de edad.");
-        }
-        System.out.println("Acceso permitido");
-    }
     
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
         Descarga d1 = new Descarga("Archivo1");
         Descarga d2 = new Descarga("Archivo2");
         Descarga d3 = new Descarga("Archivo3");
 
+        
+
         d1.start();
         d2.start();
         d3.start();
+
+        System.out.println("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        try {
+            ValidarEdad(edad);
+
+        } catch (EdadInvalidaException e) {
+            
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+
+    }
+
+    static void ValidarEdad(int edad) throws EdadInvalidaException{
+        if(edad < 18){
+            throw new EdadInvalidaException("Debe ser mayor de edad.");
+        }
+        System.out.println("Acceso permitido");
     }
     
 }
